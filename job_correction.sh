11881a87a50849713939d74972987b71cb546556
putuhendrawd <putuhendrawd@gmail.com>
2024-05-15 05:57:24 +0000

    update new log naming method

--- job_correction.sh
+++ job_correction.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+#################### basic input ####################
 #wkt1=`date -d "1 day ago" --utc +'%Y%m%d'`
 #wkt2=`date -d "3 day ago" --utc +'%Y-%m-%d'`
-wkt1=20240405
+wkt1=20240203
 echo "################ $wkt1 #################"
-# nohup python3 sqes_v3_processing.py $wkt1 verbose > log/$wkt1.log 2> error/$wkt1.err &
-nohup python3 sqes_v3_multiprocessing.py $wkt1 verbose > log/m-$wkt1.log 2> error/m-$wkt1.err &
+#################### naming #########################
+name="$wkt1"
+if [[ -e log/m-$name.log || -L log/m-$name.log ]] ; then
+    i=1
+    while [[ -e log/m-$name-$i.log || -L log/m-$name-$i.log ]] ; do
+        let i++
+    done
+    name=$name-$i
+fi
+#################### running #########################
+# nohup python3 sqes_v3_processing.py $wkt1 verbose > log/$name.log 2> error/$wname.err &
+nohup python3 sqes_v3_multiprocessing.py $wkt1 verbose > log/m-$name.log 2> error/m-$name.err &
