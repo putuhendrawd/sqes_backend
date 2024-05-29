ba1e46358c38752dc3ba7aa6a9cfa61966602827
putuhendrawd <putuhendrawd@gmail.com>
2024-05-29 06:16:42 +0000

    bugfix: fix timer output

--- batch.sh
+++ batch.sh
@@ -3,7 +3,7 @@ startdate=$1
 enddate=$2
 
 echo "running batch process for sqes_v3_multiprocessing.py"
-dt1 = "$(date +"%Y-%m-%d %H:%M:%S.%3N")"
+dt1="$(date +"%Y-%m-%d %H:%M:%S.%3N")"
 echo "Start date and time: $dt1"
 ### searching for dates ###
 dates=()
@@ -27,12 +27,26 @@ do
         name=$name-$i
     fi
     ### running
-    nohup python3 sqes_v3_multiprocessing.py $date verbose > log/m-$name.log 2> error/m-$name.err &
+    nohup timeout -14400 python3 sqes_v3_multiprocessing.py $date verbose > log/m-$name.log 2> error/m-$name.err &
     pid=$!
     echo "Running $date with pid: $pid"
     wait $pid
 done
 
-dt2 = "$(date +"%Y-%m-%d %H:%M:%S.%3N")"
-echo "Start date and time: $dt2"
+dt2="$(date +"%Y-%m-%d %H:%M:%S.%3N")"
+echo "End date and time: $dt2"
+
+#### timing report
+starttime=$(date -d "${dt1}" +%s)
+endtime=$(date -d "${dt2}" +%s)
+runtime=$((endtime - starttime))
+
+days=$((runtime / 86400))
+hours=$(( (runtime % 86400) / 3600 ))
+minutes=$(( (runtime % 3600) / 60 ))
+seconds=$((runtime % 60))
+
+post=$(printf "%dd %dh %dm %ds" $days $hours $minutes $seconds)
+echo "runtime:"
+echo $post
 echo "batch process done"
