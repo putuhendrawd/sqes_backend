f8515b76e22e139d097f1bddac59ee97e0190f83
putuhendrawd <putuhendrawd@gmail.com>
2024-07-26 21:48:26 +0700

    feat: add location information in sensors table

--- sqes_backend/utils/sensors_update.py
+++ sqes_backend/utils/sensors_update.py
@@ -21,7 +21,7 @@ stations_db = pd.read_sql('select code,latitude,longitude from stations', con=en
 
 # get sensor data
 print("--------------------------------------------------------------------------------------------")
-sensor_df = pd.DataFrame(columns=['code','channel','sensor'])
+sensor_df = pd.DataFrame(columns=['code','location','channel','sensor'])
 for station in stations_db.code:
     print("Get sensor data: ",station)
     # get data
@@ -33,13 +33,14 @@ for station in stations_db.code:
     temp_df = df_list[0].copy()
     temp_df["Station/Channel"] = temp_df["Station/Channel"].str.split(" ")
     temp_df["channel"] = temp_df["Station/Channel"].apply(lambda x: x[1] if not x[1].isnumeric() else x[2])
+    temp_df["location"] = temp_df["Station/Channel"].apply(lambda x: x[1] if x[1].isnumeric() else '')
     try:
         temp_df["sensor"] = temp_df["Sensor Type"].apply(lambda x: x.split("-")).apply(lambda x: f"{x[0]}-{x[1]}")
     except:
         temp_df["sensor"] = temp_df["Sensor Type"]
     temp_df["code"] = temp_df["Station/Channel"].apply(lambda x: x[0])
     temp_df["Year"] = temp_df["Sensor Type"].apply(lambda x: x.split("-")[-1])
-    temp_df = temp_df[["code","channel","sensor"]]
+    temp_df = temp_df[["code","location","channel","sensor"]]
 
     # concanate data
     sensor_df = pd.concat([sensor_df,temp_df], ignore_index=True)
@@ -59,19 +60,28 @@ sensor_df.to_sql('sensors', con=engine2, if_exists='replace', index=False)
 print("--------------------------------------------------------------------------------------------")
 print("Details:")
 non_colocated = 0
+non_colocated_list = []
 colocated = 0
+colocated_list = []
 other = 0
+other_list = []
 for sensor in stations_db.code:
     tmp = sensor_df[sensor_df['code'] == sensor]
     if len(tmp) == 3:
         non_colocated+=1
+        non_colocated_list.append(sensor)
     elif len(tmp) == 6:
         colocated+=1
+        colocated_list.append(sensor)
     else:
         other+=1
+        other_list.append(sensor)
 
 print("non_colocated",non_colocated)
+print(non_colocated_list)
 print("colocated",colocated)
+print(colocated_list)
 print("other",other)
+print(other_list)
 print("--------------------------------------------------------------------------------------------")
 print("process finish")
